df157536f2555e0d099e6da80b7d5b8d062ebf36
Monoke Kumu <monoke.kumu@illation.co.za>
2023-03-09 03:47:05 +0200

    Updated on 09-03-2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {},
-  plugins: [],
+  darkMode: "class",
+  content: ["./src/**/QA_Index.html"],
+  theme: {
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      animation: {
+        wavey: "wavey 1000ms linear infinite",
+      },
+    },
+  },
+  plugins: [
+    require("./plugins/openVariant"),
+    require("./plugins/animationDelay"),
+    require("./plugins/tableCaption"),
+    require("./plugins/buttonPlugin"),
+    require("./plugins/outlineSolid"),
+  ],
 };
